3a89248cd6f333330de38f9c32b21a9fb524b51f
Fatih Kadir Akın <fakin@protein.tech>
2017-02-24 01:56:45 +0300

    add subscriptions endpoints

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'graphql'
+require 'redis'
 require 'cgi'
+
+redis = Redis.new(write_timeout: 0.5)
 
 def random_id
   8.times.collect{[*'a'..'z'].sample}.join
@@ -28,7 +31,7 @@ QueryType = GraphQL::ObjectType.define do
 
   field :allTodos do
     type types[TodoType]
-    description "Find a Post by ID"
+    description "Find a ToDo by ID"
     resolve ->(obj, args, ctx) { todos }
   end
 end
@@ -43,6 +46,7 @@ MutationType = GraphQL::ObjectType.define do
     resolve ->(obj, args, ctx) {
       todo = Todo.new(random_id, args[:text], false)
       todos << todo
+      Redis.new.publish "graphql.todoAdded", JSON.dump({id: todo.id})
       todo
     }
   end
@@ -53,7 +57,10 @@ MutationType = GraphQL::ObjectType.define do
     argument :status, !types.Boolean
     resolve ->(obj, args, ctx) {
       todos.each do |todo|
-        todo.isCompleted = args[:status] if todo[:id] == args[:id]
+        if todo[:id] == args[:id]
+          todo.isCompleted = args[:status]
+          Redis.new.publish "graphql.todoUpdated", JSON.dump({id: args[:id]})
+        end
       end
       todos
     }
@@ -63,6 +70,7 @@ MutationType = GraphQL::ObjectType.define do
     type types[TodoType]
     argument :id, !types.ID
     resolve ->(obj, args, ctx) {
+      Redis.new.publish "graphql.todoRemoved", JSON.dump({id: args[:id]})
       todos = todos.reject do |todo|
         todo[:id] == args[:id]
       end
@@ -70,13 +78,82 @@ MutationType = GraphQL::ObjectType.define do
   end
 end
 
+SubscriptionType = GraphQL::ObjectType.define do
+  name "Subscription"
+  description "The subscription root of this schema"
+
+  field :todoAdded do
+    type TodoType
+    argument :id, !types.ID
+    resolve ->(obj, args, ctx) {
+      todos.select do |todo|
+        if todo[:id] == args[:id]
+          todo
+        else
+          nil
+        end
+      end.first
+    }
+  end
+
+  field :todoRemoved do
+    type TodoType
+    argument :id, !types.ID
+    resolve ->(obj, args, ctx) {
+      Todo.new(args[:id], "", false)
+    }
+  end
+
+  field :todoUpdated do
+    type TodoType
+    argument :id, !types.ID
+    resolve ->(obj, args, ctx) {
+      todos.select do |todo|
+        if todo[:id] == args[:id]
+          todo
+        else
+          nil
+        end
+      end.first
+    }
+  end
+end
+
 Schema = GraphQL::Schema.define do
   query QueryType
   mutation MutationType
+  subscription SubscriptionType
 end
 
 get '/' do
   erb :index
+end
+
+Stream = Struct.new("Stream", :id, :buffer)
+streams = []
+
+get '/graphql' do
+  content_type 'text/event-stream'
+  id = SecureRandom.hex
+  fields = GraphQL::Query.new(Schema, params[:query]).selected_operation.selections.map(&:name)
+
+  stream :keep_open do |buffer|
+    buffer << "id: #{id}\n"
+    buffer << "event: subscribed\n"
+    buffer << "data:\n\n"
+    streams << Stream.new(id, buffer)
+    redis.subscribe(fields.map{|field| "graphql.#{field}"}) do |on|
+      on.message do |channel, data|
+        variables = JSON.parse(data) rescue {}
+        result = Schema.execute(params[:query], variables: variables)
+        streams.each do |str|
+          str.buffer << "id: #{str.id}\n"
+          str.buffer << "event: #{channel}\n"
+          str.buffer << "data: #{JSON.dump(result.to_h)}\n\n"
+        end
+      end
+    end
+  end
 end
 
 post '/graphql' do
@@ -88,7 +165,6 @@ post '/graphql' do
     payload = {}
     payload["query"] = data['query'][0]
     payload["variables"] = JSON.parse(data['variables'][0]) rescue {}
-    p payload
   end
   Schema.execute(payload['query'], variables: payload['variables']).to_json
 end
